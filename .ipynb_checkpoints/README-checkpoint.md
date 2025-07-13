fb41f300efb17d3f270cd1f54b503e187b172620
Abhishek Chattaraj <a.chattaraj@login11.ufhpc>
2025-07-12 22:42:34 -0400

    Updated the submit scripts

---
+++ .ipynb_checkpoints/README-checkpoint.md
@@ -0,0 +1,15 @@
+# POSYDON on HiPerGator: A User Guide
+This page serves as a guide to assist researchers in running the [POSYDON](https://posydon.org) binary population synthesis code on the HiPerGator supercomputing cluster. It includes instructions for submitting cluster jobs, performing population analyses, and managing related workflows.
+
+
+
+## Table of Contents
+
+Tutorials
+- [How to Run a Binary Population](Tutorials/how_to_run_binary_population.md)
+- *(More tutorials coming soon!)*
+
+
+
+> 💡 **Tip**: Make sure you’ve set up your POSYDON environment on HiPerGator before following the tutorials. For help with environment setup, see the [POSYDON installation guide](https://posydon.org/POSYDON/development/getting-started/installation-guide.html).
+
--- Population_Run_Template/submit_script_run.sh
+++ Population_Run_Template/submit_script_run.sh
@@ -2,12 +2,15 @@
 
 #SBATCH --account=jeffrey.andrews
 #SBATCH --partition=hpg-milan
+#SBATCH -N 1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
 #SBATCH --array=0-199
 #SBATCH -t 48:00:00
 #SBATCH --mem-per-cpu=8G
 #SBATCH --job-name=1e+00_Zsun
-#SBATCH --output=run_pop.out
-#SBATCH --error=run_pop.err                                                                                                      
+#SBATCH --output=run_pop_%A_%a.out
+#SBATCH --error=run_pop_%A_%a.err                                                                                                      
 #SBATCH --mail-type=ALL                                                                                                            
 #SBATCH --mail-user=a.chattaraj@ufl.edu
 
@@ -16,6 +19,10 @@ export PATH_TO_POSYDON_DATA=/blue/jeffrey.andrews/POSYDON_popsynth_data/v2/25052
 
 date 
 
-srun python run_metallicity.py 1.
+# OPTION 1: Run with srun
+# srun python run_metallicity.py 1.
+
+# OPTION 2: Run with python directly
+python run_metallicity.py 1.
 
 date
